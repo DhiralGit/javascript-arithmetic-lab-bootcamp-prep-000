976eb7df78c36bea0c87b1ef059b93a0af0b2a86
Dhiral Patel <dhiralpatel794@gmail.com>
2018-04-17 12:36:38 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,26 @@
+function add(a=5, b=6) {
+  return a + b;
+}
+add()
+
+function subtract(a=5, b=6) {
+  return a - b;
+}
+subtract()
+
+function multiply(a=5, b=6) {
+  return a * b;
+}
+multiply()
+
+function divide(a=5, b=6) {
+  return a / b;
+}
+divide()
+
+function inc(){
+
+  n++;
+  return n;
+}
+inc()
